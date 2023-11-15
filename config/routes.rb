dad4fd1c0fdbd416aab827f995b75a6bba38733e
okari <nyandika15@gmail.com>
2023-11-15 20:45:12 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/show'
+  get 'recipe_foods/modify'
+  get 'recipe_foods/destroy'
   devise_for :users
-  
   root "users#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get '/recipes/public', to: 'recipes#public'
-  resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only:[:index, :new, :create, :destroy]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/recipes/public', to: 'recipes#public'
+  
+  resources :foods, only: [:index, :new, :create, :destroy, :show]
+  
+  resources :recipes, except: [:edit, :update] do
+    patch :public_toggle, on: :member
+    get 'public', on: :collection
+    get 'general_shopping_list', on: :member
+
+    resources :recipe_foods, only: [:new, :create, :destroy] do
+      patch :modify, on: :member
+    end
+  end
 end
