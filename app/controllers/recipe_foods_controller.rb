1662da1d0b35c3faf22421cd8b60e59872130ab5
okari <nyandika15@gmail.com>
2023-11-15 20:44:27 +0300

    Add RecipeFood Controller

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,58 @@
+class RecipeFoodsController < ApplicationController
+  def show
+    recipe_food = RecipeFood.find(params[:id])
+    redirect_to recipe_path(recipe_food.recipe)
+  end
+
+  def new
+    @user = current_user
+    @foods = @user.foods
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.new
+    @ingredients = []
+    @foods.each do |food|
+      @ingredients << ("#{food.name} in #{food.measurement_unit}")
+    end
+  end
+
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    quantity = params[:recipe_food][:quantity]
+    food_unit = params[:recipe_food][:food]
+    food_name = food_unit.split.first
+    food = Food.find(params[:recipe_food][:food_id])
+
+    recipe_food = RecipeFood.new(food:, recipe:, quantity:)
+    puts food.id
+    if recipe_food.save
+      flash[:success] = 'Object successfully created'
+      redirect_to recipe_path(recipe)
+    else
+      flash[:error] = 'Failed to create the recipe food'
+      render 'new'
+    end
+  end
+
+  def modify
+    recipe_food = RecipeFood.find(params[:id])
+    new_quantity = params[:recipe_food][:quantity]
+
+    if recipe_food.update(quantity: new_quantity)
+      flash[:success] = 'Quantity updated successfully'
+    else
+      flash[:error] = 'Failed to update quantity'
+    end
+
+    redirect_to recipe_food_path(recipe_food)
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.destroy
+      flash[:notice] = 'Food was successfully deleted.'
+    else
+      flash[:alert] = 'Failed to delete food.'
+    end
+    redirect_to recipes_path, notice: 'Food was successfully deleted.'
+  end
+end
