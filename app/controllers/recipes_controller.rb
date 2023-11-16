5faa3769d4893be8cd6a8a7a8e31e87e36562cae
MargaretTechworld <margarettechworld@gmail.com>
2023-11-16 09:25:38 +0000

    Creat a general shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -35,6 +35,26 @@ class RecipesController < ApplicationController
     render 'public'
   end
 
+  def general_shopping_list
+    @user = current_user
+    @recipe = Recipe.includes(:recipe_foods).find_by(id: params[:id])
+    @recipe_foods = RecipeFood.where(recipe_id: @recipe.id)
+    @needed_items = []
+    @recipe_foods.each do |recipe_food|
+      existed_food = @user.foods.find_by(name: recipe_food.food.name)
+      if existed_food.nil?
+        @needed_items << [recipe_food.food.name, recipe_food.quantity, recipe_food.food.price,
+                          recipe_food.food.measurement_unit]
+      else
+        difference_quantity = recipe_food.quantity - existed_food.quantity
+        if difference_quantity.positive?
+          @needed_items << [recipe_food.food.name, difference_quantity, existed_food.price,
+                            existed_food.measurement_unit]
+        end
+      end
+    end
+  end
+
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
