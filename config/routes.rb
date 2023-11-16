65c92cca363b1cd60ca39e7cb69d782a62eb1646
okari <nyandika15@gmail.com>
2023-11-16 10:20:49 +0300

    Fix routes error

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
-  get 'recipe_foods/new'
-  get 'recipe_foods/create'
-  get 'recipe_foods/show'
-  get 'recipe_foods/modify'
-  get 'recipe_foods/destroy'
   devise_for :users
   root "users#index"
 
-  get '/recipes/public', to: 'recipes#public'
-  
+  get '/recipes/public'
   resources :foods, only: [:index, :new, :create, :destroy, :show]
-  
   resources :recipes, except: [:edit, :update] do
-    patch :public_toggle, on: :member
-    get 'public', on: :collection
-    get 'general_shopping_list', on: :member
-
-    resources :recipe_foods, only: [:new, :create, :destroy] do
-      patch :modify, on: :member
+    member do
+      patch :public_toggle
     end
+  end
+  resources :recipes, except: %i[edit update] do
+    resources :recipe_foods, except: %i[edit update]
+    collection do
+      get 'public'
+    end
+    member do
+      get 'general_shopping_list'
+    end
+  end
+  resources :recipe_foods, only: [:new, :create, :destroy] do
+    patch :modify, on: :member
   end
 end
